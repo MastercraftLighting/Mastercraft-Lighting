5f950a60aea68c298ba4061d623058ebfdbc2f96
Deanna Warren <mrs.deanna.warren@gmail.com>
2016-01-30 10:47:12 -0800

    Test associations manually, several are broken

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,5 +1,7 @@
 class Equipment < ActiveRecord::Base
   belongs_to :production
-  belongs_to :venue, through: :equipment_venues
+  # has many below should probably be a belongs_to but it was complaining because of through which
+  # we need because of it belonging to both production and venues
+  has_many :venue, through: :equipment_venues
   has_many :equipment_notes
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,12 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 # Seed User table
-users = [{username: 'Sparky15', password: 'lights', user_type: 'Designer'},
-         {username: 'FoxyFox', password: 'grayfox', user_type: 'ME'},
-         {username: 'Adam', password: 'password', user_type: 'Administrator'},
-         {username: 'LarryLighter', password: '1234', user_type: 'Designer'},
-         {username: 'JerryCrew', password: 'monkey', user_type: 'Lead'},
-         {username: 'JohnCrew', password: 'banana', user_type: 'Lead'}]
+users = [{username: 'Sparky15',email: "Sparky15@email.com", password: 'lights', user_type: 'Designer'},
+         {username: 'FoxyFox',email: "FoxyFox@email.com", password: 'grayfox', user_type: 'ME'},
+         {username: 'Adam',email: "Adam@email.com", password: 'password', user_type: 'Administrator'},
+         {username: 'LarryLighter',email: "LarryLighter@email.com", password: '1234', user_type: 'Designer'},
+         {username: 'JerryCrew',email: "JerryCrew@email.com", password: 'monkey', user_type: 'Lead'},
+         {username: 'JohnCrew',email: "JohnCrew@email.com", password: 'banana', user_type: 'Lead'}]
 
 users.each do |user|
   User.create(user)
@@ -23,3 +23,29 @@ venues.each do |venue|
 end
 
 # Seed Production Table
+productions = [{name: 'BatBoy', date: Date.today, designer_id: 1, master_electrician_id: 2},
+               {name: 'Phantom of the Opera', date: Date.today, designer_id: 3, master_electrician_id: 4},
+               {name: 'Wicked', date: Date.today, designer_id: 5, master_electrician_id: 6}]
+
+productions.each do |production|
+  Production.create(production)
+end
+
+# Production/Venue connection table??
+
+# Equipment Seed Table
+equipment = [{instrument_type: "LightingRig1", production_id: 1},
+             {instrument_type: "LightingRig2", production_id: 2},
+             {instrument_type: "LightingRig3", production_id: 3}]
+
+equipment.each do |piece|
+  Equipment.create(piece)
+end
+
+equipment_notes = [{note: "This is a note on LightingRig1", equipment_id: 1, field_type: "instrument_type"},
+                   {note: "This is a note on LightingRig2", equipment_id: 2, field_type: "instrument_type"},
+                   {note: "This is a note on LightingRig3", equipment_id: 3, field_type: "instrument_type"}]
+
+equipment_notes.each do |note|
+  EquipmentNote.create(note)
+end
