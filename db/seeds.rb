86e52727166a19a3aea2539f3131212849329a12
Deanna Warren <mrs.deanna.warren@gmail.com>
2016-01-29 22:06:09 -0800

    Add parital seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+CsvInput.create()
+
+User.create(
+      email: "butt@monkey.com"
+      password:"password"
+      user_type: "0"
+      username: "Buttmonkey"
+)
+
+EquipmentNote.create(
+      t.text :note: ""
+      t.integer :equipment_id, null: false
+      t.string :field_type, null: false
+)
+
+Equipment.create(
+      t.integer :production_id, null: false
+)
+
+Lead.create(
+      t.integer :production_id, null: false
+      t.integer :user_id, null: false
+)
+
+ProductionVenue.create(
+      t.integer :venue_id, null: false
+      t.integer :production_id, null: false
+)
+
+EquipmentVenue.create(
+      t.integer :equipment_id, null: false
+      t.integer :venue_id, null: false
+)
+
+Production.create(
+      t.string :name, null: false
+)
+
+Venue.create(
+      t.string :name, null: false
+)
