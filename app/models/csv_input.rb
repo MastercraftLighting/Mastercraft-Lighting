92a4555eb2025b52f9237837c34d1d1dca603d9e
Deanna Warren <mrs.deanna.warren@gmail.com>
2016-01-30 20:28:51 -0800

    Add working csv to database route, production id on equipment is static at 20 for now

--- app/models/csv_input.rb
+++ app/models/csv_input.rb
@@ -1,30 +1,30 @@
 class CsvInput < ActiveRecord::Base
-  # after_create :infer_equipment
+  after_create :infer_equipment
 
 
 
   private
-    # def infer_equipment
-    #   Equipment.create(instrument_type: self.instrument_type,
-    #      wattage: self.wattage,
-    #      purpose: self.purpose,
-    #      position: self.position,
-    #      unit_number: self.unit_number,
-    #      color: self.color,
-    #      dimmer: self.dimmer,
-    #      channel: self.channel,
-    #      address: self.address,
-    #      universe: self.universe,
-    #      circuit_number: self.circuit_number,
-    #      circuit_name: self.circuit_name,
-    #      frame_size: self.frame_size,
-    #      gobo_1: self.gobo_1,
-    #      gobo_2: self.gobo_2,
-    #      focus: self.focus,
-    #      accessories: self.accessories
-    #    #production_id: self.production_id
-    #    #will need done elsewhere, possibly returning the data to a view
-    #      )
-    # end
+    def infer_equipment
+      Equipment.create(instrument_type: self.instrument_type,
+         wattage: self.wattage,
+         purpose: self.purpose,
+         position: self.position,
+         unit_number: self.unit_number,
+         color: self.color,
+         dimmer: self.dimmer,
+         channel: self.channel,
+         address: self.address,
+         universe: self.universe,
+         circuit_number: self.circuit_number,
+         circuit_name: self.circuit_name,
+         frame_size: self.frame_size,
+         gobo_1: self.gobo_1,
+         gobo_2: self.gobo_2,
+         focus: self.focus,
+         accessories: self.accessories,
+       production_id: 20
+       #will need done elsewhere, possibly returning the data to a view
+         )
+    end
 
 end
