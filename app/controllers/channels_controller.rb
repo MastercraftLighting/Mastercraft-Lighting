24bd1bd2edc7753e7191ed130cc5f24be0b71637
Bill Brinkert <email": null>
2016-01-30 21:32:44 -0800

    Start process of building out modal form to find_or_create channel by

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,2 +1,39 @@
 class ChannelsController < ApplicationController
+  before_action :set_channel, only: [:show, :update, :destroy]
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_channel
+    equipment = Production.find(params[:prod_id])
+    @channel = equipment.find(params[:id])
+  end
+
 end
