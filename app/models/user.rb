d333951606f0213e7be848a91fe1c0e8bc6a667d
Deanna Warren <mrs.deanna.warren@gmail.com>
2016-01-31 12:51:06 -0800

    Fix user relations designed_productions and master_electrician_productions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   has_many :leads
   has_many :lead_productions, through: :leads, source: "user"
-  has_many :designed_productions, foreign_key: :designer_id, source: "user"
-  has_many :master_electrician_productions, foreign_key: :master_electrician_id, source: "user"
+  has_many :designed_productions, foreign_key: :designer_id, class_name: "Production"
+  has_many :master_electrician_productions, foreign_key: :master_electrician_id, class_name: "Production"
 
 end
