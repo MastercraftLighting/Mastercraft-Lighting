87ff8a9cd3f652f9367a849de9e46cf33a10f6e2
Bill Brinkert <email": null>
2016-01-30 17:48:31 -0800

    Basic routes to show production have been written

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,12 +1,46 @@
 class ProductionsController < ApplicationController
-	def index
-	end
+  before_action :set_production, only: [:show, :udpate, :destroy]
 
-	def new
-		@production = Production.new(params)
-		if @production.save
-			redirect_to :new
-		else
-			redirect_to :index
-	end
+  def index
+    @productions = Production.all
+
+    render :index
+  end
+
+  def create
+
+  end
+
+  def new
+    @production = Production.new(params)
+    if @production.save
+      redirect_to :new
+    else
+      redirect_to :index
+    end
+  end
+
+  def edit
+
+  end
+
+  def show
+    @equipment = @production.equipment
+    render :new
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def set_production
+      @production = Production.find(params[:id])
+    end
+
 end
