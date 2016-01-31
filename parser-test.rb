9c94d331ed1eb4dbd04c6ad295a2352cc4ee9452
Jon Clayton <Jonclayton@swbell.net>
2016-01-30 20:19:13 -0800

    Delete unneeded files for CSV reading

--- parser-test.rb
+++
@@ -1,9 +0,0 @@
-require 'csv'
-require 'smarter_csv'
-
-input_string = File.read("test.tsv").force_encoding('MacRoman').encode('UTF-8')
-none_string = input_string.gsub(/\"/, '')
-data = CSV.parse(none_string, :col_sep => "\t")
-puts data.length
-p data
-
