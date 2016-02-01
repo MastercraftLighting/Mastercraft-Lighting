fccf98ef75722bce5d31d443f3aa30da55fb9f48
Jon Clayton <Jonclayton@swbell.net>
2016-02-01 15:01:28 -0800

    Move upload download to production list page

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -6,12 +6,12 @@ class UploadsController < ApplicationController
   end
 
   def create
-    @production = Production.find(params[:production_id])
+    @production = Production.find(params[:id])
     input_string = params[:vectorworks_file].read.force_encoding('MacRoman').encode('UTF-8')
     none_string = input_string.gsub(/\"/, '')
     imported_data = CSV.parse(none_string, :col_sep => "\t")
     CsvInput.build(imported_data,@production.id)
-    render "productions/show" #
+    redirect_to production_path(@production.id)
   end
 
   def download
