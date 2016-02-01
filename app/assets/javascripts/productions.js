fbae7e5351fbdbc8ddea5c95401a0fd836930439
Bill Brinkert <email": null>
2016-02-01 10:02:59 -0800

    Fix fail response functionality of ajax calls

--- app/assets/javascripts/productions.js
+++ app/assets/javascripts/productions.js
@@ -71,7 +71,9 @@ var deleteRow = function(path){
     $('#CK-Row' + response.rowNumber).remove();
     $('#DM-Row' + response.rowNumber).remove();
     $('#IS-Row' + response.rowNumber).remove();
-  }).fail(console.log("ajax delete call failed "));
+  }).fail(function(response){
+    console.log("ajax delete call failed: " + response);
+  });
 };
 
 var editRow = function(path){
@@ -82,7 +84,9 @@ var editRow = function(path){
   }).done(function(response){
     console.log("Removing item:" );
     $('#CH-Row' + response.rowNumber).remove();
-  }).fail(console.log("ajax delete call failed "))
+  }).fail(function(response){
+    console.log("ajax delete call failed: " + response);
+  });
 };
 
 var getConfirmation = function(){
