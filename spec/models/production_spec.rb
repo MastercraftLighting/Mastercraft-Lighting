00a056979a50fbbd709ec561fb2d7d48e65f1420
Deanna Warren <mrs.deanna.warren@gmail.com>
2016-02-01 18:20:22 -0800

    Add tests

--- spec/models/production_spec.rb
+++ spec/models/production_spec.rb
@@ -1,5 +1,37 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Production, :type => :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Production, :type => :model do
+
+  describe "Create" do
+    context "when valid" do
+      it 'can be created' do
+        production = Production.create(name: "Production")
+        expect(production.id).to be_truthy
+      end
+    end
+    context "when not valid" do
+      xit 'cannot be created' do
+        expect(Production.create).to raise_error
+      end
+    end
+  end
+
+  # describe "Read" do
+
+  # end
+
+  # describe "Update" do
+  #   context "when valid" do
+  #     it 'can be updated' do
+  #     end
+  #   end
+  #   context "when not valid" do
+  #     it 'cannot be updated'
+  #   end
+  # end
+
+  # describe "Destroy" do
+
+  # end
+
+end
