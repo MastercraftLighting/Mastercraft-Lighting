a3c88a5016e99a0e0e2163dae2d929e80185df89
Jon Clayton <Jonclayton@swbell.net>
2016-02-01 16:09:42 -0800

    Add cleaning of data. No more nils.  No more promises of love

--- app/models/csv_input.rb
+++ app/models/csv_input.rb
@@ -8,11 +8,12 @@ class CsvInput < ActiveRecord::Base
         header.downcase.gsub(/ /,"_").gsub(/&&/,"and")
     end
     imported_data.each do |line|
-        new_record = self.create(production_id: production_id)
         line_params = {production_id: production_id}
         headers.each_with_index do |header, index|
-            line_params.update(header => line[index])
+            line_params.update(header => line[index].to_s)
         end
+        puts line_params
+        puts CsvInput.count
         self.create(line_params)
     end
 end
@@ -30,26 +31,29 @@ end
 
   private
     def infer_equipment
-      Equipment.create(
-         instrument_type: self.instrument_type,
-         wattage: self.wattage,
-         purpose: self.purpose,
-         position: self.position,
-         unit_number: self.unit_number,
-         color: self.color,
-         dimmer: self.dimmer,
-         channel: self.channel,
-         address: self.address,
-         universe: self.universe,
-         circuit_number: self.circuit_number,
-         circuit_name: self.circuit_name,
-         frame_size: self.frame_size,
-         gobo_1: self.gobo_1,
-         gobo_2: self.gobo_2,
-         focus: self.focus,
-         accessories: self.accessories,
-         production_id: self.production_id
-         )
+        Equipment.import(self)
+      # item = Equipment.new(
+      #    instrument_type: self.instrument_type,
+      #    wattage: self.wattage,
+      #    purpose: self.purpose,
+      #    position: self.position,
+      #    unit_number: self.unit_number,
+      #    color: self.color,
+      #    dimmer: self.dimmer,
+      #    channel: self.channel,
+      #    address: self.address,
+      #    universe: self.universe,
+      #    circuit_number: self.circuit_number,
+      #    circuit_name: self.circuit_name,
+      #    frame_size: self.frame_size,
+      #    gobo_1: self.gobo_1,
+      #    gobo_2: self.gobo_2,
+      #    focus: self.focus,
+      #    accessories: self.accessories,
+      #    production_id: self.production_id
+      #    )
+
+
     end
 
 end
