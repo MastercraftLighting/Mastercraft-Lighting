54ffd2554471349a00e3c1b84e35453fac8f3d15
Jon Clayton <Jonclayton@swbell.net>
2016-02-03 12:16:25 -0800

    deleting puts

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -20,17 +20,9 @@ class Equipment < ActiveRecord::Base
   # This is extremely fragile.  Users have to enter their equipment name in exactly the expected format to get a match
   before_save do
     if self.instrument_type.slice(-3,3) == "deg"
-      puts "this is self:"
-      p self
       unit_name = self.instrument_type.slice(0..-7)
-      puts "this is unit_name"
-      p unit_name
       beam_angle = self.instrument_type.slice(-5,2)
-      puts "this is beam_angle"
-      p beam_angle
       lookup_instrument = UnitLibrary.find_by(unit_name: unit_name, beam_angle: beam_angle)
-      puts "this is lookup_instrument"
-      p lookup_instrument
       if (self.frame_size == nil || self.frame_size == "") && lookup_instrument
         self.frame_size = lookup_instrument.frame_size || ""
       end
